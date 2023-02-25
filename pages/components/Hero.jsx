6f0534ccfa559ee4a0d3feff58eff67894393036
shahrokh11 <sky2iran@gmail.com>
2023-02-25 18:21:56 +0330

    update all

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
     const { data: totalMinted, isLoading: totalMintedLoading } = useContractRead(contract, "totalMinted")
 
     function handleIncrement() {
-        setCount(count >= 1 ? 1 : count + 1);
+        setCount(count >= 50 ? 50 : count + 1);
     }
 
     function handleDecrement() {
